d23a5e8c58daceab307e6404133ecc6c95caca7a
jakir9324 <jakirhusen9324@gmail.com>
2025-02-04 18:38:17 +0600

    js,conditional statement

---
+++ script.js
@@ -0,0 +1,38 @@
+
+
+104
+
+let number = 104;
+
+if(number > 0){
+    console.log("the number is positive");
+}
+else if(number > 0){
+    console.log("the number is nagetive");
+}
+
+//Write a js program to find maximum between two numbers.\
+
+99 , 78
+
+let number1 = 99;
+let number2 = 78;
+
+if(number1 > number2){
+    console.log("number1 is maximum");
+}
+else if(number1 > number2){
+    console.log("number2 is maximum");
+}
+
+
+//write a js program to chech whether a number is divisible by 5.
+
+let number3 = 5;
+
+if(number3 % 5 == 0){
+    console.log("number3 is divisible by 5");
+}
+else{
+    console.log("number3 is not divisible by 5");
+}
